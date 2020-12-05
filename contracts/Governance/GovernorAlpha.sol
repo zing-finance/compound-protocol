133c50c114bdda687a052d14cc203509cbc86bc1
chainvisions <67360117+chainvisions@users.noreply.github.com>
2020-12-04 22:41:27 -0700

    Update GovernorAlpha.sol

--- contracts/Governance/GovernorAlpha.sol
+++ contracts/Governance/GovernorAlpha.sol
@@ -3,13 +3,13 @@ pragma experimental ABIEncoderV2;
 
 contract GovernorAlpha {
     /// @notice The name of this contract
-    string public constant name = "Compound Governor Alpha";
+    string public constant name = "Xdbx Lending Governor Alpha";
 
     /// @notice The number of votes in support of a proposal required in order for a quorum to be reached and for a vote to succeed
-    function quorumVotes() public pure returns (uint) { return 400000e18; } // 400,000 = 4% of Comp
+    function quorumVotes() public pure returns (uint) { return 400000e18; } // 400,000 = 4% of xLEND
 
     /// @notice The number of votes required in order for a voter to become a proposer
-    function proposalThreshold() public pure returns (uint) { return 100000e18; } // 100,000 = 1% of Comp
+    function proposalThreshold() public pure returns (uint) { return 100000e18; } // 100,000 = 1% of xLEND
 
     /// @notice The maximum number of actions that can be included in a proposal
     function proposalMaxOperations() public pure returns (uint) { return 10; } // 10 actions
